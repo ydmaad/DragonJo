860ba8ca3e008d55e54625ae6124777792b09f14
kimyong <right4570@naver.com>
2024-05-31 21:48:39 +0900

    Layout파일 추가 footer&header파일추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Router from './routes/Router';
+import Router from './routes/router';
 
 function App() {
 
