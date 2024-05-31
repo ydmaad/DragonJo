5bb1e538fc9967f54185a22adcfe1fd64cc6c89b
kimyong <right4570@naver.com>
2024-05-31 22:10:24 +0900

    오류남

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom/dist';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from '../components/Layout';
 
-import AuthPage from '../pages/AuthPage/AuthPage';
-import HomePage from '../pages/HomePage/HomePage';
-
-const Router = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/auth" element={<AuthPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
