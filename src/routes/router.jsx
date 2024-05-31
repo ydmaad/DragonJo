db5509f619272856923726390ff927fa26f0352c
kimyong <right4570@naver.com>
2024-05-31 22:20:59 +0900

    update Layout

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom/dist';
 
 import AuthPage from '../pages/AuthPage/AuthPage';
 import HomePage from '../pages/HomePage/HomePage';
-
+import Layout from '../components/Layout';
 const Router = () => (
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/auth" element={<AuthPage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </Layout>
   </BrowserRouter>
 );
 
