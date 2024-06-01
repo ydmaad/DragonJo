447879007c9557ec8e487b59bd495e4000c841f8
sangjun <987654321123sin@gmail.com>
2024-06-02 00:51:17 +0900

    Chore: key 수정 중

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yzkoayeawivyvwgpnzvu.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6a29heWVhd2l2eXZ3Z3BuenZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxNTczMjEsImV4cCI6MjAzMjczMzMyMX0.v7vxRW7a8xOhF0n2c9dcwr6pTu7PZp9x748xcpdbdZA';
-
+const supabaseUrl = import.meta.env.VITE_PROJECT_URL;
+const supabaseKey = import.meta.env.VITE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 const Mypage = () => {
-  return (
-    <>
-        
-    </>
-  )
+  return <></>;
 };
 
 export default Mypage;
