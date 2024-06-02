e1e0e4a86301c4eb83a9681e4d86b8d4fcd0d0e4
kimyong <right4570@naver.com>
2024-06-02 15:13:59 +0900

    Design: mypage ui 배치

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -7,7 +7,7 @@ const Mypage = () => {
       <S.MyProfile>
         <div className="profile-photo">
           <S.profileId>
-            <div className='profile-box'>
+            <div className="profile-box">
               <img src={profileIcon} alt="profileIcon" />
             </div>
             <div>
@@ -17,7 +17,54 @@ const Mypage = () => {
           </S.profileId>
         </div>
         <div className="profile-info">
-          <h3>회원정보</h3>
+          <h3 className='profile-title'>회원정보</h3>
+          <h3 className='profile-subtitle'>회원정보 설정</h3>
+          <S.Mypagetable>
+            <tbody>
+              <tr>
+                <S.MypagetdTitle>회원번호</S.MypagetdTitle>
+                <S.MypageContents>
+                  <div>
+                    <h3>1234567890</h3>
+                  </div>
+                </S.MypageContents>
+              </tr>
+              <tr>
+                <S.MypagetdTitle>아이디</S.MypagetdTitle>
+                <S.MypageContents>
+                  <div>
+                    <h3>result@naver.com</h3>
+                  </div>
+                </S.MypageContents>
+              </tr>
+              <tr>
+                <S.MypagetdTitle>비밀번호</S.MypagetdTitle>
+                <S.MypageContents>
+                  <div>
+                    <h3>pass******</h3>
+                    <button>변경</button>
+                  </div>
+                </S.MypageContents>
+              </tr>
+              <tr>
+                <S.MypagetdTitle>닉네임</S.MypagetdTitle>
+                <S.MypageContents>
+                  <div>
+                    <h3>honghonghong</h3>
+                    <button>변경</button>
+                  </div>
+                </S.MypageContents>
+              </tr>
+              <tr>
+                <S.MypagetdTitle>탈퇴신청</S.MypagetdTitle>
+                <S.MypageContents>
+                  <div>
+                    <button>탈퇴</button>
+                  </div>
+                </S.MypageContents>
+              </tr>
+            </tbody>
+          </S.Mypagetable>
         </div>
       </S.MyProfile>
     </>
