904349b1545f0e5c0cd21dc62013493b6df25d0e
kimyong <right4570@naver.com>
2024-06-06 11:03:59 +0900

    Feat: 마이페이지에서 유저본인이 작성한 포스트내의 댓글갯수를 카운팅해서 ui배치함

--- src/pages/Mypage/MypostListItem.jsx
+++ src/pages/Mypage/MypostListItem.jsx
@@ -4,9 +4,27 @@ import ddabong from '../../assets/ddabong.png';
 import * as S from '../../styledComponents/Mypost';
 import { useNavigate } from 'react-router-dom';
 import { PostContent } from '../HomePage/HomePage.styles';
+import { useEffect, useState } from 'react';
+import { supabase } from '../../service/supabase';
 
 const MypostListItem = (props) => {
+  const [commentsCount, setCommentsCount] = useState(0);
   const navigate = useNavigate();
+  const getCommentsTotalLength = async () => {
+    let { data: comments, error } = await supabase
+      .from('comments')
+      .select('*', { count: 'exact' })
+      .eq('post_id', props.postId);
+    if (error) {
+      console.error('토탈 댓글 수 계산실패=>', error);
+      return;
+    }
+    setCommentsCount(comments.length);
+  };
+  useEffect(() => {
+    getCommentsTotalLength();
+  }, []);
+
   return (
     <S.MypostListItem
       onClick={() => {
@@ -24,11 +42,11 @@ const MypostListItem = (props) => {
         <S.CommentsIconBox className="comments-box">
           <div>
             <img src={comments} alt="commentsIcon" />
-            <p style={{marginTop:'3px'}}>0</p>
+            <p style={{ marginTop: '3px' }}>{commentsCount}</p>
           </div>
           <div>
-            <img src={ddabong} style={{height:'24px'}} alt="ddabongIcon" />
-            <p style={{marginTop:'3px'}}>0</p>
+            <img src={ddabong} style={{ height: '24px' }} alt="ddabongIcon" />
+            <p style={{ marginTop: '3px' }}>0</p>
           </div>
         </S.CommentsIconBox>
       </div>
