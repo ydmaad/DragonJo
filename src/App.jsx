e9344e1f518d84ac95a48b42011e3e7201e646d6
butterbeetle <butterbeetle@naver.com>
2024-06-06 18:27:22 +0900

    Fix: 배포 사이트에서 문제가 생겨 AutoLogout 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { useAutoLogout } from './hooks/useAutoLogout';
 import { fetchPosts } from './redux/slices/postSlice';
 import { setUser } from './redux/slices/user.slice';
 import router from './routes/router';
@@ -9,7 +8,7 @@ import { supabase } from './service/supabase';
 
 function App() {
   const dispatch = useDispatch();
-  useAutoLogout();
+  // useAutoLogout();
 
   useEffect(() => {
     const getSession = async () => {
