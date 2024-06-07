fa1e5b4c586cf126cab859174fa882511ead6bd3
butterbeetle <butterbeetle@naver.com>
2024-06-07 10:17:50 +0900

    Feat: ProctectedRoute 추가 - 로그인 여부에 따라 페이지 이동

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom/dist';
+import ProtectedRoute from '../components/ProtectedRoute';
 import Layout from '../layouts/Layout';
 import AuthPage from '../pages/AuthPage/AuthPage';
 import DetailPage from '../pages/DetailPage/DetailPage';
@@ -9,44 +10,57 @@ import Mypage from '../pages/Mypage/Mypage';
 import ResetPage from '../pages/ResetPage/ResetPage';
 import WritePostPage from '../pages/WritePostPage/WritePostPage';
 
+const pages = [
+  {
+    path: '/',
+    element: <HomePage />,
+    isLoggedInRequired: false
+  },
+  {
+    path: '/auth',
+    element: <AuthPage />,
+    isLoggedInRequired: false
+  },
+  {
+    path: '/forgot-password',
+    element: <ForgotPage />,
+    isLoggedInRequired: false
+  },
+  {
+    path: '/reset-password',
+    element: <ResetPage />,
+    isLoggedInRequired: false
+  },
+  {
+    path: '/detail/:id',
+    element: <DetailPage />,
+    isLoggedInRequired: false
+  },
+  {
+    path: '/write',
+    element: <WritePostPage />,
+    isLoggedInRequired: true
+  },
+  {
+    path: '/edit/:id',
+    element: <EditPostPage />,
+    isLoggedInRequired: true
+  },
+  {
+    path: '/mypage',
+    element: <Mypage />,
+    isLoggedInRequired: true
+  }
+];
+
 const router = createBrowserRouter([
   {
+    path: '/',
     element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />
-      },
-      {
-        path: '/auth',
-        element: <AuthPage />
-      },
-      {
-        path: '/forgot-password',
-        element: <ForgotPage />
-      },
-      {
-        path: '/reset-password',
-        element: <ResetPage />
-      },
-      {
-        path: '/write',
-        element: <WritePostPage />
-      },
-      {
-        path: '/edit/:id',
-        element: <EditPostPage />
-      },
-      {
-        path: '/mypage',
-        element: <Mypage />
-      },
-      {
-        path: '/detail/:id',
-        element: <DetailPage />
-      }
-    ]
+    children: pages.map((page) => ({
+      path: page.path,
+      element: <ProtectedRoute isLoggedInRequired={page.isLoggedInRequired}>{page.element}</ProtectedRoute>
+    }))
   }
 ]);
-
 export default router;
